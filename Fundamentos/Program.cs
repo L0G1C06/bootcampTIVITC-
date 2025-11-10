5c8bb750c2cc45964d33d26990f91ccc236594ae
EduWMaldaner <eduardo.maldaner@tivit.com>
2025-11-10 15:07:28 -0300

    ...

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,10 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Fundamentos.Models;
+﻿using Fundamentos.Models;
 
 Pessoa pessoa = new Pessoa();
 string nome = "Eduardo";
 int idade = 21;
 pessoa.Apresentar(nome, idade);
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Teste");
