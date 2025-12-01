8b25b6128b49762d08807a9841ffe208e43f3840
EduWMaldaner <eduardo.maldaner@tivit.com>
2025-12-01 14:41:49 -0300

    criando primeiros métodos

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -3,4 +3,26 @@
 Pessoa p1 = new Pessoa();
 p1.Nome = "Eduardo";
 p1.Idade = 21;
-p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Buta";
+p2.Idade = 26;
+
+Curso ingles = new Curso();
+ingles.Nome = "Inglês Básico";
+ingles.Alunos = new List<Pessoa>();
+
+ingles.AdicionarAluno(p1);
+ingles.AdicionarAluno(p2);
+
+ingles.ListarAlunos();
+
+ingles.QuantidadeAlunosMatriculados();
+
+ingles.RemoverAluno(p1);
+ingles.RemoverAluno(p2);
+
+ingles.ListarAlunos();
+
+ingles.QuantidadeAlunosMatriculados();
+// p1.Apresentar();
