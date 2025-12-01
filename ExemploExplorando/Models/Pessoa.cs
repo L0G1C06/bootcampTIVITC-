fc02d1e32d79de47b2cf3cc7f31984ab3b41e406
EduWMaldaner <eduardo.maldaner@tivit.com>
2025-12-01 14:11:18 -0300

    criando primeira propriedade

---
+++ ExemploExplorando/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; } // tendo get;set é automaticamente idenficiado como Propriedade de uma classe
+        public int Idade { get; set; }
+    }
+}
