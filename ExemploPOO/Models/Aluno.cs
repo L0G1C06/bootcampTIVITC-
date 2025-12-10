46a86b4cfa3c0aa95377a6f176ff5da869e8c923
EduWMaldaner <eduardo.maldaner@tivit.com>
2025-12-10 11:38:45 -0300

    feat: herança implementada

---
+++ ExemploPOO/Models/Aluno.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    // Aluno herda da classe Pessoa usando ':'
+    public class Aluno : Pessoa
+    {
+        public double Nota { get; set; }
+    }
+}
--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -8,7 +8,22 @@
 // p1.Apresentar();
 
 // Exemplo de encapsulamento. Onde o método 'Sacar' manipula o atributo 'saldo' que é privado.
-ContaCorrente c1 = new ContaCorrente(123, 1000);
-c1.ExibirSaldo();
-c1.Sacar(5000);
-c1.ExibirSaldo();
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+// c1.ExibirSaldo();
+// c1.Sacar(5000);
+// c1.ExibirSaldo();
+
+// Exemplo de Herança - Aluno herda atributos e métodos da classe Pessoa
+Aluno a1 = new Aluno();
+a1.Nome = "Ana Silva";
+a1.Idade = 19;
+a1.Email = "ana.silva@gmail.com";
+a1.Nota = 8.5;
+a1.Apresentar();
+
+Professor prof1 = new Professor();
+prof1.Nome = "Carlos Souza";
+prof1.Idade = 40;
+prof1.Email = "carlos.souza@gmail.com";
+prof1.Disciplina = "Matemática";
+prof1.Apresentar();
