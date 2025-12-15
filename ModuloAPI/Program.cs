222502133d06bf3f871b4347bd2dd6147428007b
EduWMaldaner <eduardo.maldaner@tivit.com>
2025-12-15 11:42:44 -0300

    usando sqlite como banco de dados para a aplicação

--- ModuloAPI/Program.cs
+++ ModuloAPI/Program.cs
@@ -1,6 +1,13 @@
+using ModuloAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ConexaoPadrao"))
+);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
