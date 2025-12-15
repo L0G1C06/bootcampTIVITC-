4913957ffcf36f6d797d7f4b971ff923e6b83389
EduWMaldaner <eduardo.maldaner@tivit.com>
2025-12-15 13:41:00 -0300

    prática iniciada usando projeto template

---
+++ Pratica4GerenciadorTarefas/Program.cs
@@ -0,0 +1,26 @@
+using ModuloAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ConexaoPadrao"))
+);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
